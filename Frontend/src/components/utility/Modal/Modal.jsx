b420b3e2127b860178ee5224dc9d5d111b1089bf
FevenSeyfu <fevenseyfu00@gmail.com>
2024-10-31 23:17:03 +0300

    add reusable modal

---
+++ Frontend/src/components/utility/Modal/Modal.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Modal from 'react-modal';
+import { IoMdClose } from 'react-icons/io';
+
+Modal.setAppElement('#root');
+
+const ModalComponent = ({ modalLabel, isOpen, onRequestClose, children }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      className="fixed inset-0 flex items-center justify-center z-50"
+      overlayClassName="fixed inset-0 bg-black bg-opacity-25"
+      contentLabel={modalLabel}
+    >
+      <div className="relative bg-white text-black rounded-xl shadow-md shadow-light-purple p-6 flex flex-col w-2/3 max-w-lg mx-auto">
+        <button
+          onClick={onRequestClose}
+          className="absolute top-4 right-4 text-dark-blue hover:text-white hover:bg-dark-blue rounded-md"
+        >
+          <IoMdClose size={30} />
+        </button>
+        {children}
+      </div>
+    </Modal>
+  );
+};
+
+export default ModalComponent;
