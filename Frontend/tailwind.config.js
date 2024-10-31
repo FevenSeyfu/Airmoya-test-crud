3b27d1f2cbee87060d9037e98a255ff26b882f8c
FevenSeyfu <fevenseyfu00@gmail.com>
2024-10-31 19:47:19 +0300

    setup basic folder strucutre and files

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        'light-purple': '#b9bfff',
+        'purple': '#8b74bd',
+        'darkblue': '#22277a',
+        'neutral-bg': '#DDDDDD',
+        'neutral-text': '#B3B3B3',
+        'success': '#027E46',
+        'success-light': '#E6F2EC',
+        'info': '#005EFF',
+        'info-light': '#F5F9FF',
+        'warning': '#3b372f',
+        'warning-light': '#FEF7E7',
+        'error': '#E11900',
+        'error-light': '#FCE8E6',
+      },
+    },
+  },
+  plugins: [],
+}
+
